6eba4cfc3dd0dca7d0c9d28f98e0db4c54cab270
ADVAN <mahdijanlou@gmail.com>
2016-07-13 12:04:53 +0430

    Add new file

--- plugins/tosticker.lua
+++ plugins/tosticker.lua
@@ -1,7 +1,7 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'adv/stickers/sticker.webp'
+    local file = 'system/adv/stickers/sticker.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
