5a02a0c85008c14cc022c692eb77a4ee8630564b
ADVAN <mahdijanlou@gmail.com>
2016-07-14 16:00:45 +0430

    Auto launch has been fixed!
    Thanks to @Realod :)

--- steady.sh
+++ steady.sh
@@ -69,7 +69,7 @@ NONVOLUNTARYCHECK=0
 VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
-BOT=teleadvan  # You can put here other bots. Also you can change it to run more than one bot in the same server.
+BOT=AdvanSource  # You can put here other bots. Also you can change it to run more than one bot in the same server.
 RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
@@ -96,7 +96,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by CRUEL M.KH                 \e[00;37;40m"
+echo -e "               \e[01;34m                    AdvanSource                 \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
