ec7ff08247133abbbedebc4460bae0def1e62195
ADVAN <mahdijanlou@gmail.com>
2016-07-16 17:11:19 +0430

    updated!

--- steady.sh
+++ steady.sh
@@ -248,7 +248,7 @@ cat << EOF
  
 EOF
 echo -e "                \e[100m                Steady script           \e[00;37;40m"
-echo -e "               \e[01;34m                    by CRUEL M.KH                 \e[00;37;40m"
+echo -e "               \e[01;34m                    AdvanSource                 \e[00;37;40m"
 echo ""
 cat << EOF
  $bld$f1▄ ▀▄   ▄▀ ▄   $f2 ▄▄▄████▄▄▄    $f3  ▄██▄     $f4▄ ▀▄   ▄▀ ▄   $f5 ▄▄▄████▄▄▄    $f6  ▄██▄  $rst
