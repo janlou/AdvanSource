ca882bf6ae9f231883fcdaa182aedc17073a08b0
ADVAN <mahdijanlou@gmail.com>
2016-07-20 10:09:22 +0430

    Update "language.lua"

--- plugins/language.lua
+++ plugins/language.lua
@@ -48,7 +48,7 @@ function run(msg, matches)
 end
 
   if matches[1] == "help" and matches[2] == "fa" then
-    file1 = io.open("./langs/help-fa", "r")
+    file1 = io.open("./langs/helpSUPER-fa", "r")
    	local send1 = file1:read("*all")
     local b = 1
     while b ~= 0 do
@@ -61,7 +61,7 @@ end
       filea:close()
 	 return "زبان هلپ ربات در سوپرگپ تغییر کرد\nحالا شما میتوانید از دستور !help استفاده کنید"
  elseif matches[1] == "help" and matches[2] == "en" then
-    file2 = io.open("./langs/help-en", "r")
+    file2 = io.open("./langs/helpSUPER-en", "r")
     local send2 = file2:read("*all")
     local b = 1
     while b ~= 0 do
@@ -74,7 +74,7 @@ end
       fileb:close()
 	 return "Language of help in supergroup has been changed,\nNow you can send !help commands"
   elseif matches[1] == "help" and matches[2] == "فا" then
-    file3 = io.open("./langs/help-فا", "r")
+    file3 = io.open("./langs/helpSUPER-فا", "r")
     local send3 = file3:read("*all")
     local b = 1
     while b ~= 0 do
@@ -87,7 +87,92 @@ end
       filec:close()
 	 return "زبان هلپ سوپرگپ به فارسی با دستورات فارسی تغییر کرد\nحالا شما میتوانید از دستور !help استفاده کنید"
  end
+end
+
+if msg.to.type == 'chat' then
+ if matches[1] == "setlang" and matches[2] == "fa" then
+    file1 = io.open("./langs/ingroup-fa", "r")
+   	local send1 = file1:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send1 = send1:trim()
+    send1,b = send1:gsub('^!+','')
+	end
+      filea = io.open("./plugins/ingroup.lua", "w")
+      filea:write(send1)
+      filea:flush()
+      filea:close()
+	 return "زبان گپ معمولی با موفقیت تغییر کرد\nبرای اعمال تغییرات از دستور زیر استفاده کنید:\n!update"
+ elseif matches[1] == "setlang" and matches[2] == "en" then
+    file2 = io.open("./langs/ingroup-en", "r")
+    local send2 = file2:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send2 = send2:trim()
+    send2,b = send2:gsub('^!+','')
+	end
+      fileb = io.open("./plugins/ingroup.lua", "w")
+      fileb:write(send2)
+      fileb:flush()
+      fileb:close()
+	 return "Chat language has been changed\nFor the change of commands please use:\n!update"
+ elseif matches[1] == "setlang" and matches[2] == "فا" then
+    file3 = io.open("./langs/ingroup-فا", "r")
+   	local send3 = file3:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send3 = send3:trim()
+    send3,b = send3:gsub('^!+','')
+	end
+      filec = io.open("./plugins/ingroup.lua", "w")
+      filec:write(send3)
+      filec:flush()
+      filec:close()
+       return "زبان گپ معمولی با موفقیت به فارسی با دستورات فارسی تغییر کرد\nبرای اعمال تغییرات از دستور زیر استفاده کنید:\n!update"
+end
+
+  if matches[1] == "help" and matches[2] == "fa" then
+    file1 = io.open("./langs/helpCHAT-fa", "r")
+   	local send1 = file1:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send1 = send1:trim()
+    send1,b = send1:gsub('^!+','')
+	end
+      filea = io.open("./helps/HelpChat.txt", "w")
+      filea:write(send1)
+      filea:flush()
+      filea:close()
+	 return "زبان هلپ ربات در گپ معمولی تغییر کرد\nحالا شما میتوانید از دستور !help استفاده کنید"
+ elseif matches[1] == "help" and matches[2] == "en" then
+    file2 = io.open("./langs/helpCHAT-en", "r")
+    local send2 = file2:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send2 = send2:trim()
+    send2,b = send2:gsub('^!+','')
+	end
+      fileb = io.open("./helps/HelpChat.txt", "w")
+      fileb:write(send2)
+      fileb:flush()
+      fileb:close()
+	 return "Language of help in chat has been changed,\nNow you can send !help commands"
+  elseif matches[1] == "help" and matches[2] == "فا" then
+    file3 = io.open("./langs/helpCHAT-فا", "r")
+    local send3 = file3:read("*all")
+    local b = 1
+    while b ~= 0 do
+    send3 = send3:trim()
+    send3,b = send3:gsub('^!+','')
+	end
+      filec = io.open("./helps/HelpChat.txt", "w")
+      filec:write(send3)
+      filec:flush()
+      filec:close()
+	 return "زبان هلپ گپ معمولی به فارسی با دستورات فارسی تغییر کرد\nحالا شما میتوانید از دستور !help استفاده کنید"
  end
+ end
+
  if matches[1] == "update" then
   txt = "Updated!"
   send_msg("channel#id"..msg.to.id, txt, ok_cb, false)
