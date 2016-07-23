feb0a37eba597aa2841e0e0b819de788acece8f6
ADVAN <mahdijanlou@gmail.com>
2016-07-23 16:57:23 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,10 +3,10 @@ do
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'leave' and is_creator(msg) then
+    if matches[1] == 'leave' and is_admin1(msg) or is_creator(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
-    elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
+    elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) or not is_creator(msg) then
        send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
