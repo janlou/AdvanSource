e119285e83278d68ae6a2f3e97cbd54f87722109
ADVAN <mahdijanlou@gmail.com>
2016-07-23 16:58:16 +0430

    Update pm.lua

--- plugins/pm.lua
+++ plugins/pm.lua
@@ -1,8 +1,10 @@
 function run(msg, matches)
+	if is_sudo(msg) or is_admin1(msg) then
 if matches[1] == "pm" then
     	local text = "Message From "..(msg.from.username or msg.from.last_name).."\n\nMessage : "..matches[3]
     	send_large_msg("user#id"..matches[2],text)
     	return "Message has been sent"
+end
 end
 end
 return {
