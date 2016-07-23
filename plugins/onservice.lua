16b2f2ff19e9f2f3113c4dbb49fa09f6edf80b15
ADVAN <mahdijanlou@gmail.com>
2016-07-23 16:56:46 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,7 +3,7 @@ do
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'leave' and is_admin1(msg) then
+    if matches[1] == 'leave' and is_creator(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
