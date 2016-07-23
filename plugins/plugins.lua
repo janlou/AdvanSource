faaadd0c3b789f0542ca9848ec858d40897adc95
ADVAN <mahdijanlou@gmail.com>
2016-07-23 16:43:55 +0430

    updated!

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -152,7 +152,7 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == '!plugins' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == '!plugins' and is_creator(msg) then --after changed to moderator mode, set only sudo
     return list_all_plugins()
   end
 
@@ -165,7 +165,7 @@ local function run(msg, matches)
   end
 
   -- Enable a plugin
-  if matches[1] == '+' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == '+' and is_creator(msg) then --after changed to moderator mode, set only sudo
     local plugin_name = matches[2]
     print("enable: "..matches[2])
     return enable_plugin(plugin_name)
@@ -180,7 +180,7 @@ local function run(msg, matches)
   end
 
   -- Disable a plugin
-  if matches[1] == '-' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == '-' and is_creator(msg) then --after changed to moderator mode, set only sudo
     if matches[2] == 'plugins' then
     	return 'This plugin can\'t be disabled'
     end
@@ -189,7 +189,7 @@ local function run(msg, matches)
   end
 
   -- Reload all the plugins!
-  if matches[1] == '*' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == '*' and is_creator(msg) then --after changed to moderator mode, set only sudo
     return reload_plugins(true)
   end
 end
