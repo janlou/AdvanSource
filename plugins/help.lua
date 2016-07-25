fe09aa7ae67889fc7955e94261242b8c20b118cb
ADVAN <mahdijanlou@gmail.com>
2016-07-25 21:15:18 +0430

    Source has been fixed!

--- plugins/help.lua
+++ plugins/help.lua
@@ -8,16 +8,20 @@ function run(msg, matches)
 	
 if is_momod(msg) or is_owner(msg) or is_sudo(msg) or is_vip(msg) then
 	if matches[1] == "help" then
-	if msg.to.type == 'channel' then
+	if msg.to.type == 'channel' and is_super_group(msg) then
 	HelpSuper = io.open("./helps/HelpSuper.txt", "r")
     	help_text_super = HelpSuper:read("*all")
 	  return help_text_super
+	elseif msg.to.type == 'channel' and not is_super_group(msg) then
+	  return "SupreGroup is not added!"
 	end
 	
-    if msg.to.type == 'chat' then
+    if msg.to.type == 'chat' and is_group(msg) then
 	HelpChat = io.open("./helps/HelpChat.txt", "r")
     	help_text = HelpChat:read("*all")
 	  return help_text
+	 elseif msg.to.type == 'chat' and not is_group(msg) then
+	  return "Group is not added!"
 	end
 	end
  elseif not is_momod(msg) or not is_owner(msg) or not is_vip(msg) then
