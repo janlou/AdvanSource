fb4aa95ceb24a7d3c550b5a40ea7936347d83163
ADVAN <mahdijanlou@gmail.com>
2016-08-13 17:57:09 +0430

    important

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -118,7 +118,7 @@ end
 function pre_process_msg(msg)
   for name,plugin in pairs(plugins) do
     if plugin.pre_process and msg then
-      print('پیش پردازش:', name)
+      print('Preprocess: ', name)
       msg = plugin.pre_process(msg)
     end
   end
@@ -157,7 +157,7 @@ function match_plugin(plugin, plugin_name, msg)
   for k, pattern in pairs(plugin.patterns) do
     local matches = match_pattern(pattern, msg.text)
     if matches then
-      print("msg matches: ", pattern)
+      print("Msg pattern: ", pattern)
 
       if is_plugin_disabled_on_chat(plugin_name, receiver) then
         return nil
@@ -247,6 +247,16 @@ function create_config( )
     sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
     support_gp = {111111111},--Support id
     moderation = {data = 'system/data/adv.json'},
+    about_text = [[*IN THE NAME OF ALLAH*
+This is an original bot and based on (AdvanSource).
+Copyright all right reserved and you must respect all laws.
+
+Source: https://github.com/janlou/AdvanSource
+Channel: @AdvanTm
+Messenger: @Advanbot
+Creator: @janlou
+Site: http://StoreVps.ir
+]]
   }
   serialize_to_file(config, './system/data/config.lua')
   print('saved config into ./system/data/config.lua')
