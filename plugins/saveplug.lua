38ea2e299ea273e99f30b46f6f6d21f842303185
ADVAN <mahdijanlou@gmail.com>
2016-09-08 13:28:43 +0430

    Update 'saveplug.lua' and add auto enable

--- plugins/saveplug.lua
+++ plugins/saveplug.lua
@@ -1,16 +1,54 @@
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
+local function plugin_enabled( name )
+  for k,v in pairs(_config.enabled_plugins) do
+    if name == v then
+      return k
+    end
+  end
+  return false
+end
+
+local function disable_plugin( name, chat )
+  local k = plugin_enabled(name)
+  if not k then
+    return
+  end
+  table.remove(_config.enabled_plugins, k)
+  save_config( )
+end
+
+local function enable_plugin( plugin_name )
+  if plugin_enabled(plugin_name) then
+    return disable_plugin( name, chat )
+  end
+    table.insert(_config.enabled_plugins, plugin_name)
+    save_config()
+end
+
+local function plugin_exists( name )
+  for k,v in pairs(plugins_names()) do
+    if name..'.lua' == v then
+      return true
+    end
+  end
+  return false
+end
+  
+local function reload_plugins( )
+  plugins = {}
+  load_plugins()
+end
 
 local function saveplug(extra, success, result)
   local msg = extra.msg
   local name = extra.name
   local receiver = get_receiver(msg)
   if success then
-    local file = 'plugins/'..name..'.lua'
+    local file = 'plugins-self/'..name..'.lua'
     print('File saving to:', result)
     os.rename(result, file)
     print('File moved to:', file)
+    enable_plugin(name)
+    reload_plugins( )
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
@@ -28,12 +66,14 @@ load_document(msg.reply_id, saveplug, {msg=msg,name=name})
 end
 end
 return {
+  advan = {
+    "Created by: @janlou & @Alirezame",
+    "Powered by: @SUDO_TM & @AdvanTM",
+    "CopyRight all right reserved",
+  },
   patterns = {
- "^[!/#](save) (.*)$",
+   "^[!/#](save) (.*)$",
   },
   run = run,
 }
 
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
