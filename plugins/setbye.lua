ef4dfa0c827620d30fb721294dbf6321f41a89c9
ADVAN <mahdijanlou@gmail.com>
2016-09-08 14:25:47 +0430

    Fixed!

--- plugins/setbye.lua
+++ plugins/setbye.lua
@@ -2,6 +2,9 @@ local function run(msg, matches)
  local text = matches[2]
  if is_momod(msg) or is_owner(msg) or is_sudo(msg) then
  if matches[1] == "setbye" and matches[2] then
+  if not is_momod(msg) or not is_owner(msg) or not is_sudo(msg) then
+   return "فقط مخصوص ادمین ها"
+  end
    local b = 1
   while b ~= 0 do
     text = text:trim()
@@ -13,22 +16,18 @@ local function run(msg, matches)
   file:close()
   return "متن خروج کاربر تغییر کرد به:\n"..text
  end
-elseif not is_momod(msg) or not is_owner(msg) or not is_sudo(msg) then
- return "فقط مخصوص ادمین ها"
 end
    
    local bye = io.open("./system/chats/logs/"..msg.to.id..".txt", "r")
    local send = bye:read("*all")
    if matches[1] == "chat_del_user" or matches[1] == "channel_kick" or matches[1] == "kick_user" then
       return send
-    else
-     print("Bye text not found")
    end
    
   if is_momod(msg) or is_owner(msg) or is_sudo(msg) then
    local say = "متن خروج با موفقیت حذف شد"
    if matches[1] == "delbye" then
-    del = io.popen("cd adv && cd bye && rm "..msg.to.id..".txt")
+    del = io.popen("cd system/chats/logs && rm "..msg.to.id..".txt")
     send_msg(get_receiver(msg), say, ok_cb, false)
    return del
    end
