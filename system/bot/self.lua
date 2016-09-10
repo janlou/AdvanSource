d69cff38852c8c5d6e1b49729756445cda8f5ab5
ADVAN <mahdijanlou@gmail.com>
2016-09-10 14:31:31 +0430

    important

--- system/bot/self.lua
+++ system/bot/self.lua
@@ -212,7 +212,8 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-     "Filemanager",
+    	 "answer",
+         "Filemanager",
 	 "FunTools",
 	 "groupmanager",
 	 "help",
