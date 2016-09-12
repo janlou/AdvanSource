548aec2fc545020863541a7aafc2ddd828270252
ADVAN <mahdijanlou@gmail.com>
2016-09-12 14:34:53 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -2,24 +2,32 @@ function run(msg, matches)
 	
     if is_realm(msg) then
     if is_sudo(msg) or is_vip(msg) then
-     return "Realm haven`t help\nRealm is a place for sudo users!"
+     return "Realm hasn`t help\nRealm is a place for sudo users!"
 	end
 	end
 	
 if is_momod(msg) or is_owner(msg) or is_sudo(msg) or is_vip(msg) then
-	if matches[1] == "help" then
-	if msg.to.type == 'channel' and is_super_group(msg) then
-	HelpSuper = io.open("./helps/HelpSuper.txt", "r")
-    	help_text_super = HelpSuper:read("*all")
-	  return help_text_super
-	elseif msg.to.type == 'channel' and not is_super_group(msg) then
-	  return "SupreGroup is not added!"
-	end
+  if matches[1] == "help" then
+  if msg.to.type == 'channel' and is_super_group(msg) then
+    one = io.open("./system/adv/team", "r")
+    two = io.open("./system/adv/channel", "r")
+    local team = one:read("*all")
+    local channel = two:read("*all")
+  HelpSuper = io.open("./helps/HelpSuper.txt", "r")
+  help_text_super = HelpSuper:read("*all")
+    send_msg("channel#id"..msg.to.id, help_text_super..'\nPowered by '..team..'\n<a href="'..channel..'">Join our channel :)</a>', ok_cb, false)
+  elseif msg.to.type == 'channel' and not is_super_group(msg) then
+    return "SupreGroup is not added!"
+  end
 	
     if msg.to.type == 'chat' and is_group(msg) then
+    one = io.open("./system/adv/team", "r")
+    two = io.open("./system/adv/channel", "r")
+    local team = one:read("*all")
+    local channel = two:read("*all")
 	HelpChat = io.open("./helps/HelpChat.txt", "r")
     	help_text = HelpChat:read("*all")
-	  return help_text
+    send_msg("chat#id"..msg.to.id, help_text..'\nPowered by '..team..'\n<a href="'..channel..'">Join our channel :)</a>', ok_cb, false)
 	 elseif msg.to.type == 'chat' and not is_group(msg) then
 	  return "Group is not added!"
 	end
