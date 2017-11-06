a0b3da93dde0acd8a9b06002daf8eff9b72fc6bb
lashguy <33408878+lashguy@users.noreply.github.com>
2017-11-06 07:22:00 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {478665745},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
